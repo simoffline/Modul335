7a4e978d1b9ddf3657abff3fa30a89735693dc31
unknown <simon.pfister@stud.edubs.ch>
2019-02-27 08:28:34 +0100

    first commit

---
+++ OneWay/OneWay/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Locations;
+using Android.Gms.Maps;
+
+using System;
+
+namespace OneWay
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            TextView Latitude = FindViewById<TextView>(Resource.Id.LatitudeNum);
+            TextView Longtitude = FindViewById<TextView>(Resource.Id.LongtitudeNum);
+            TextView Altitude = FindViewById<TextView>(Resource.Id.AltitudeNum);
+            Button GetLocation = FindViewById<Button>(Resource.Id.GetLocation);
+           
+        
+
+        }
+    }
+}
